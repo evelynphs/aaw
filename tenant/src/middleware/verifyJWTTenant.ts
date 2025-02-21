5bdb02b51950f95bac1f223c3f57a0c5b2ccdf66
evelynphs <evelyn.p.h.silalahi@gmail.com>
2025-02-21 11:42:54 +0700

    tenant done w/o docker

---
+++ tenant/src/middleware/verifyJWTTenant.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UnauthenticatedResponse } from "../commons/patterns/exceptions";
+import axios, { AxiosResponse } from "axios";
+import { getTenantService } from "@src/tenant/services";
+
+const VERIFY_ADMIN_TOKEN_URL = "http://localhost:8000/api/auth/verify-admin-token"
+
+interface JWTUser extends JwtPayload {
+  id: string;
+  tenant_id: string;
+}
+
+export const verifyJWTTenant = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization?.split("Bearer ")[1];
+    if (!token) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+
+    const payload = await axios.post(`${VERIFY_ADMIN_TOKEN_URL}`, { token });
+    if (payload.status !== 200) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+
+    const verifiedPayload = payload as {
+      status: 200;
+      data: {
+        user: {
+          id: string | null;
+          username: string;
+          email: string;
+          full_name: string | null;
+          address: string | null;
+          phone_number: string | null;
+        };
+      };
+    }
+
+    req.body.user = verifiedPayload.data.user;
+    next();
+  } catch (error) {
+    return res.status(401).json(
+      new UnauthenticatedResponse("Invalid token").generate()
+    );
+  }
+};
